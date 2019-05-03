e83f1493b6a0e16221832693a0fae8ff3d073871
sbenning <samuel.benning@zento.fr>
2019-05-03 17:40:07 +0200

    plop

--- src/apollo/zto-mongodb-graphql/models/mongodb-queries.ts
+++ src/apollo/zto-mongodb-graphql/models/mongodb-queries.ts
@@ -81,13 +81,13 @@ export type QueryModels<Model> = {
     sort?: QuerySort<Model>;
 } & QueryModel<Model>;
 
-export const queryPaginationGQLinputDef = gql`
+export const queryPaginationGQLinputDef = `
 input QueryPaginationInput {
     limit: Int!
     skip: Int
 }
 `;
-export const queryStringGQLinputDef = gql`
+export const queryStringGQLinputDef = `
 input QueryStringInput {
     AND: [QueryStringInput!]
     OR: [QueryStringInput!]
@@ -100,7 +100,7 @@ input QueryStringInput {
     NIN: [String!]
 }
 `;
-export const queryStringItemGQLinputDef = gql`
+export const queryStringItemGQLinputDef = `
 input QueryStringItemInput {
     AT: Int!
     AND: [QueryStringInput!]
@@ -114,7 +114,7 @@ input QueryStringItemInput {
     NIN: [String!]
 }
 `;
-export const queryStringArrayGQLinputDef = gql`
+export const queryStringArrayGQLinputDef = `
 input QueryStringArrayInput {
     SIZE: QueryIntInput
     ITEMS: [QueryStringItemInput!]
@@ -129,7 +129,7 @@ input QueryStringArrayInput {
     NIN: [String!]
 }
 `;
-export const queryBooleanGQLinputDef = gql`
+export const queryBooleanGQLinputDef = `
 input QueryBooleanInput {
     AND: [QueryBooleanInput!]
     OR: [QueryBooleanInput!]
@@ -142,7 +142,7 @@ input QueryBooleanInput {
     NIN: [Boolean!]
 }
 `;
-export const queryBooleanItemGQLinputDef = gql`
+export const queryBooleanItemGQLinputDef = `
 input QueryBooleanItemInput {
     AT: Int!
     AND: [QueryBooleanInput!]
@@ -156,7 +156,7 @@ input QueryBooleanItemInput {
     NIN: [Boolean!]
 }
 `;
-export const queryBooleanArrayGQLinputDef = gql`
+export const queryBooleanArrayGQLinputDef = `
 input QueryBooleanArrayInput {
     SIZE: QueryIntInput
     ITEMS: [QueryBooleanItemInput!]
@@ -171,7 +171,7 @@ input QueryBooleanArrayInput {
     NIN: [Boolean!]
 }
 `;
-export const queryIntGQLinputDef = gql`
+export const queryIntGQLinputDef = `
 input QueryIntInput {
     AND: [QueryIntInput!]
     OR: [QueryIntInput!]
@@ -188,7 +188,7 @@ input QueryIntInput {
     LTE: Int
 }
 `;
-export const queryIntItemGQLinputDef = gql`
+export const queryIntItemGQLinputDef = `
 input QueryIntItemInput {
     AT: Int!
     AND: [QueryIntInput!]
@@ -206,7 +206,7 @@ input QueryIntItemInput {
     LTE: Int
 }
 `;
-export const queryIntArrayGQLinputDef = gql`
+export const queryIntArrayGQLinputDef = `
 input QueryIntArrayInput {
     SIZE: QueryIntInput
     ITEMS: [QueryIntItemInput!]
@@ -230,51 +230,207 @@ export function capitalize(str: string) {
     return str.trim().slice(0, 1).toUpperCase() + str.trim().slice(1).toLowerCase();
 }
 
-export type Dependency = {
+export type _Dependency = {
+    sort?: any;
     input?: any;
+    type?: any;
+    changes?: any;
+    update?: any;
     item?: any;
     array?: any;
+    root?: any;
+    typeName?: string;
+    changesName?: string;
+    updateName?: string;
     inputName?: string;
     itemName?: string;
+    sortName?: string;
     arrayName?: string;
+    inputTypeName?: string;
+    inputType?: any;
+    gql?: any;
+    queries?: any;
+    mutations?: any;
+    args?: any;
+    argsRequired?: any;
 };
+
+export type Dependency = _Dependency | string
 export type Dependencies = { [x: string]: Dependency };
 
 export function makeModelFieldInputDef<Model>(property: string, field: Model, dependencies: Dependencies = {}) {
     // field is a primitive string
     if (typeof(field) === 'string') {
-        return `${property}: QueryStringInput
-        `;
+        return `
+    ${property}: QueryStringInput`;
     // field is a primitive number
     } else if (typeof(field) === 'number') {
-        return `${property}: QueryIntInput
-        `;
+        return `
+    ${property}: QueryIntInput`;
     // field is a primitive boolean
     } else if (typeof(field) === 'boolean') {
-        return `${property}: QueryBooleanInput
-        `;
+        return `
+    ${property}: QueryBooleanInput`;
     } else if (Array.isArray(field)) {
         // field is an array of primitive string
         if (typeof(field[0]) === 'string') {
-            return `${property}: QueryStringArrayInput
-            `;
+            return `
+    ${property}: QueryStringArrayInput`;
         // field is an array of primitive number
         } else if (typeof(field[0]) === 'number') {
-            return `${property}: QueryIntArrayInput
-            `;
+            return `
+    ${property}: QueryIntArrayInput`;
         // field is an array of primitive boolean
         } else if (typeof(field[0]) === 'boolean') {
-            return `${property}: QueryBooleanArrayInput
-            `;
+            return `
+    ${property}: QueryBooleanArrayInput`;
         // field is an array of models
         } else {
-            return `${property}: ${dependencies[property].arrayName}
-            `;
+            const initialDependency = dependencies[property] as string;
+            return `
+    ${property}: ${(dependencies[initialDependency] as _Dependency).arrayName}`;
         }
     // field is a model
     } else {
-        return `${property}: ${dependencies[property].inputName}
-        `;
+            const initialDependency = dependencies[property] as string;
+            return `
+    ${property}: ${(dependencies[initialDependency] as _Dependency).inputName}`;
+    }
+}
+
+export function makeModelFieldTypeDef<Model>(property: string, field: Model, dependencies: Dependencies = {}) {
+    // field is a primitive string
+    if (typeof(field) === 'string') {
+        return `
+    ${property}: String!`;
+    // field is a primitive number
+    } else if (typeof(field) === 'number') {
+        return `
+    ${property}: Int!`;
+    // field is a primitive boolean
+    } else if (typeof(field) === 'boolean') {
+        return `
+    ${property}: Boolean!`;
+    } else if (Array.isArray(field)) {
+        // field is an array of primitive string
+        if (typeof(field[0]) === 'string') {
+            return `
+    ${property}: [String!]!`;
+        // field is an array of primitive number
+        } else if (typeof(field[0]) === 'number') {
+            return `
+    ${property}: [Int!]!`;
+        // field is an array of primitive boolean
+        } else if (typeof(field[0]) === 'boolean') {
+            return `
+    ${property}: [Boolean!]!`;
+        // field is an array of models
+        } else {
+            const initialDependency = dependencies[property] as string;
+            return `
+    ${property}: [${(dependencies[initialDependency] as _Dependency).typeName}!]!`;
+        }
+    // field is a model
+    } else {
+            const initialDependency = dependencies[property] as string;
+            return `
+    ${property}: ${(dependencies[initialDependency] as _Dependency).typeName}!`;
+    }
+}
+
+export function makeModelFieldChangesDef<Model>(property: string, field: Model, dependencies: Dependencies = {}) {
+    // field is a primitive string
+    if (typeof(field) === 'string') {
+        return `
+    ${property}: String`;
+    // field is a primitive number
+    } else if (typeof(field) === 'number') {
+        return `
+    ${property}: Int`;
+    // field is a primitive boolean
+    } else if (typeof(field) === 'boolean') {
+        return `
+    ${property}: Boolean`;
+    } else if (Array.isArray(field)) {
+        // field is an array of primitive string
+        if (typeof(field[0]) === 'string') {
+            return `
+    ${property}: [String!]`;
+        // field is an array of primitive number
+        } else if (typeof(field[0]) === 'number') {
+            return `
+    ${property}: [Int!]`;
+        // field is an array of primitive boolean
+        } else if (typeof(field[0]) === 'boolean') {
+            return `
+    ${property}: [Boolean!]`;
+        // field is an array of models
+        } else {
+            const initialDependency = dependencies[property] as string;
+            return `
+    ${property}: [${(dependencies[initialDependency] as _Dependency).changesName}!]`;
+        }
+    // field is a model
+    } else {
+            const initialDependency = dependencies[property] as string;
+            return `
+    ${property}: ${(dependencies[initialDependency] as _Dependency).changesName}`;
+    }
+}
+
+export function getArgType(prop: string, arg, dependencies: Dependencies = {}) {
+    // field is a primitive string
+    if (typeof(arg) === 'string') {
+        return `${prop}: String`;
+    // field is a primitive number
+    } else if (typeof(arg) === 'number') {
+        return `${prop}: Int`;
+    // field is a primitive boolean
+    } else if (typeof(arg) === 'boolean') {
+        return `${prop}: Boolean`;
+    } else if (Array.isArray(arg)) {
+        // field is an array of primitive string
+        if (typeof(arg[0]) === 'string') {
+            return `${prop}: [String!]`;
+        // field is an array of primitive number
+        } else if (typeof(arg[0]) === 'number') {
+            return `${prop}: [Int!]`;
+        // field is an array of primitive boolean
+        } else if (typeof(arg[0]) === 'boolean') {
+            return `${prop}: [Boolean!]`;
+        // field is an array of models
+        } else {
+            const initialDependency = dependencies[prop] as string;
+            return `${prop}: ${(dependencies[initialDependency] as _Dependency).inputTypeName}`;
+        }
+    // field is a model
+    } else {
+            const initialDependency = dependencies[prop] as string;
+            return `${prop}: ${(dependencies[initialDependency] as _Dependency).inputTypeName}`;
+    }
+}
+
+export function getSortFieldInput(prop: string, arg, dependencies: Dependencies = {}) {
+    // field is a primitive string
+    if (typeof(arg) === 'string' || typeof(arg) === 'number' || typeof(arg) === 'boolean') {
+        return `
+    ${prop}: Int`;
+    // field is a primitive number
+    } else if (Array.isArray(arg)) {
+        if (typeof(arg[0]) === 'string' || typeof(arg[0]) === 'number' || typeof(arg[0]) === 'boolean') {
+            return `
+    ${prop}: Int`;
+        // field is an array of primitive number
+        } else {
+            const initialDependency = dependencies[prop] as string;
+            return `
+    ${prop}: ${(dependencies[initialDependency] as _Dependency).sortName}`;
+        }
+    // field is a primitive boolean
+    } else {
+            const initialDependency = dependencies[prop] as string;
+            return `
+    ${prop}: ${(dependencies[initialDependency] as _Dependency).sortName}`;
     }
 }
 
@@ -284,36 +440,151 @@ export function makeModelGQLinputDef<Model>(rawName: string, instance: Model, de
     const queryModelInputName = `Query${cName}Input`;
     const queryModelItemInputName = `Query${cName}ItemInput`;
     const queryModelArrayInputName = `Query${cName}ArrayInput`;
+    const queryTypeName = `${cName}`;
+    const queryChangesName = `${cName}ChangesInput`;
+    const queryUpdateName = `${cName}UpdateInput`;
+    const sortName = `${cName}SortInput`;
     dependencies[rawName] = {
         inputName: queryModelInputName,
         itemName: queryModelItemInputName,
         arrayName: queryModelArrayInputName,
+        typeName: queryTypeName,
+        changesName: queryChangesName,
+        updateName: queryUpdateName,
     };
-    const properiesDefs = Object.keys(instance)
-        .reduce((thisDef, property) => thisDef + makeModelFieldInputDef(property, instance[property], dependencies), '');
+    const propertiesSort = Object.keys(instance)
+        .reduce((thisDef, property, idx) => thisDef + getSortFieldInput(property, instance[property], dependencies).slice(idx ? 0 : 5), '');
+    const propertiesDefs = Object.keys(instance)
+        .reduce((thisDef, property, idx) => thisDef + makeModelFieldInputDef(property, instance[property], dependencies).slice(idx ? 0 : 5), '');
+    const propertiesTypeDefs = Object.keys(instance)
+        .reduce((thisDef, property, idx) => thisDef + makeModelFieldTypeDef(property, instance[property], dependencies).slice(idx ? 0 : 5), '');
+    const propertiesChangesDefs = Object.keys(instance)
+        .reduce((thisDef, property, idx) => thisDef + makeModelFieldChangesDef(property, instance[property], dependencies).slice(idx ? 0 : 5), '');
     const defs = `
-    input ${queryModelInputName} {
-        ${properiesDefs}
-    }
+input ${queryModelInputName} {
+    ${propertiesDefs}
+}
+`;
+    const rootDefs = `
+input ${queryModelInputName}Root {
+    ${propertiesDefs}
+    pagination: QueryPagination
+}
+`;
+    const input = `
+input ${queryTypeName}Input {
+    ${propertiesTypeDefs}
+}
+`;
+    const itemDefs = `
+input ${queryModelItemInputName} {
+    AT: Int!
+    ${propertiesDefs}
+}
     `;
+    const arrayDefs = `
+input ${queryModelArrayInputName} {
+    SIZE: QueryIntInput
+    ITEMS: [${queryModelItemInputName}!]
+    ${propertiesDefs}
+}
+    `;
+    const typeDef = `
+type ${queryTypeName} {
+    ${propertiesTypeDefs}
+}
+    `;
+    const changesDef = `
+input ${queryChangesName} {
+    ${propertiesChangesDefs}
+}
+    `;
+    const updateDef = `
+input ${queryUpdateName} {
+    id: ID!
+    changes: ${queryChangesName}!
+}
+    `;
+    const sort = `
+input ${cName}SortInput {
+    ${propertiesSort}
+}
+    `;
+    const argsRequired = Object.keys(instance).reduce((thisArgs, key) => thisArgs + (thisArgs ? `, ${getArgType(key, instance[key], dependencies)}!` : `${getArgType(key, instance[key], dependencies)}!`), '');
+    const args = Object.keys(instance).reduce((thisArgs, key) => thisArgs + (thisArgs ? `, ${getArgType(key, instance[key], dependencies)}` : `${getArgType(key, instance[key], dependencies)}`), '');
     dependencies[rawName] = {
+        root: rootDefs,
         inputName: queryModelInputName,
         itemName: queryModelItemInputName,
         arrayName: queryModelArrayInputName,
-        input: defs
+        input: defs,
+        inputTypeName: queryTypeName + 'Input',
+        inputType: input,
+        item: itemDefs,
+        array: arrayDefs,
+        sort: sort,
+        sortName: sortName,
+        typeName: queryTypeName,
+        changesName: queryChangesName,
+        updateName: queryUpdateName,
+        type: typeDef,
+        changes: changesDef,
+        update: updateDef,
+        args: args,
+        argsRequired: argsRequired,
+        gql: defs,
+        queries: `
+    get${cName}Where(query: ${queryModelInputName + 'Root'}): [${cName}!]
+    getById${cName}(id: ID!): ${cName}`,
+        mutations: `
+    create${cName}(input: ${queryTypeName + 'Input!'}): ${cName}
+    update${cName}(update: ${queryUpdateName}!): ${cName}
+    delete${cName}(id: ID!): ${cName}
+    create${cName}Many(inputs: [${queryTypeName + 'Input'}!]!): [${cName}!]
+    update${cName}Many(updates: [${queryUpdateName}!]!): [${cName}!]
+    delete${cName}Many(ids: [ID!]!): [${cName}]
+    update${cName}Where(query: ${queryModelInputName}!, updates: [${queryUpdateName}!]!): [${cName}!]
+    delete${cName}Where(query: ${queryModelInputName}!, ids: [ID!]!): [${cName}]`,
     };
-    console.log(defs);
-    return gql`${defs}`;
+    return dependencies;
 }
 
-export function makeModelsGQLinputDef<Model>(name: string, instance: Model) {
-
-    return gql`
-    extend type Query {
-
-    }
-    extend type Mutation {
-
-    }
-    `;
+export function makeModelsGQLinputDef(dependencies: Dependencies) {
+    return `
+    ${queryStringGQLinputDef}
+    ${queryStringItemGQLinputDef}
+    ${queryStringArrayGQLinputDef}
+    ${queryBooleanGQLinputDef}
+    ${queryBooleanItemGQLinputDef}
+    ${queryBooleanArrayGQLinputDef}
+    ${queryIntGQLinputDef}
+    ${queryIntItemGQLinputDef}
+    ${queryIntArrayGQLinputDef}
+${Object.keys(dependencies)
+    .filter(key => typeof(dependencies[key]) !== 'string')
+    .reduce((gqlDef, key) => gqlDef
+        + `${(dependencies[key] as _Dependency).root}`
+        + `${(dependencies[key] as _Dependency).input}`
+        + `${(dependencies[key] as _Dependency).sort}`
+        + `${(dependencies[key] as _Dependency).item}`
+        + `${(dependencies[key] as _Dependency).array}`
+        + `${(dependencies[key] as _Dependency).changes}`
+        + `${(dependencies[key] as _Dependency).update}`
+        + `${(dependencies[key] as _Dependency).inputType}`
+        + `${(dependencies[key] as _Dependency).type}`, '')
 }
+type Query {
+    ${Object.keys(dependencies)
+        .filter(key => typeof(dependencies[key]) !== 'string')
+        .reduce((gqlDef, key) => gqlDef + `${(dependencies[key] as _Dependency).queries}`, '').slice(5)
+    }
+}
+type Mutation {
+    ${Object.keys(dependencies)
+        .filter(key => typeof(dependencies[key]) !== 'string')
+        .reduce((gqlDef, key) => gqlDef + `${(dependencies[key] as _Dependency).mutations}`, '').slice(5)
+    }
+}
+`;
+}
+
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import { environment } from "./environments";
 import { Server } from "./apollo/server";
-import { makeModelGQLinputDef } from "./apollo/zto-mongodb-graphql/models/mongodb-queries";
+import { makeModelGQLinputDef, Dependencies, makeModelsGQLinputDef } from "./apollo/zto-mongodb-graphql/models/mongodb-queries";
+import { gql } from "apollo-server-core";
 
 // console.log('With envionment: ', environment);
 
@@ -52,9 +53,292 @@ const doc2: Doc2 = {
   doc1s: [doc1, { ...doc1, thisDoc0: { ...doc1.thisDoc0 } }],
 };
 
-let dependencies = {};
+let dependencies: Dependencies = {
+  thisDoc0: 'doc0',
+  doc1s: 'doc1',
+};
+
 dependencies = makeModelGQLinputDef('doc0', doc0, dependencies);
-dependencies = makeModelGQLinputDef('doc1', doc1, { ...dependencies, thisDoc0: dependencies['doc0'] });
+dependencies = makeModelGQLinputDef('doc1', doc1, dependencies);
+dependencies = makeModelGQLinputDef('doc2', doc2, dependencies);
 
-console.log('***************************************************************************************************');
+const schema = makeModelsGQLinputDef(dependencies);
+console.log(schema);
 
+gql`
+
+input QueryStringInput {
+  AND: [QueryStringInput!]
+  OR: [QueryStringInput!]
+  NOR: [QueryStringInput!]
+  EQ: String
+  NEQ: String
+  EX: String
+  NEX: String
+  IN: [String!]
+  NIN: [String!]
+}
+input QueryStringItemInput {
+  AT: Int!
+  AND: [QueryStringInput!]
+  OR: [QueryStringInput!]
+  NOR: [QueryStringInput!]
+  EQ: String
+  NEQ: String
+  EX: String
+  NEX: String
+  IN: [String!]
+  NIN: [String!]
+}
+input QueryStringArrayInput {
+  SIZE: QueryIntInput
+  ITEMS: [QueryStringItemInput!]
+  AND: [QueryStringInput!]
+  OR: [QueryStringInput!]
+  NOR: [QueryStringInput!]
+  EQ: String
+  NEQ: String
+  EX: String
+  NEX: String
+  IN: [String!]
+  NIN: [String!]
+}
+input QueryBooleanInput {
+  AND: [QueryBooleanInput!]
+  OR: [QueryBooleanInput!]
+  NOR: [QueryBooleanInput!]
+  EQ: Boolean
+  NEQ: Boolean
+  EX: Boolean
+  NEX: Boolean
+  IN: [Boolean!]
+  NIN: [Boolean!]
+}
+input QueryBooleanItemInput {
+  AT: Int!
+  AND: [QueryBooleanInput!]
+  OR: [QueryBooleanInput!]
+  NOR: [QueryBooleanInput!]
+  EQ: Boolean
+  NEQ: Boolean
+  EX: Boolean
+  NEX: Boolean
+  IN: [Boolean!]
+  NIN: [Boolean!]
+}
+input QueryBooleanArrayInput {
+  SIZE: QueryIntInput
+  ITEMS: [QueryBooleanItemInput!]
+  AND: [QueryBooleanInput!]
+  OR: [QueryBooleanInput!]
+  NOR: [QueryBooleanInput!]
+  EQ: Boolean
+  NEQ: Boolean
+  EX: Boolean
+  NEX: Boolean
+  IN: [Boolean!]
+  NIN: [Boolean!]
+}
+input QueryIntInput {
+  AND: [QueryIntInput!]
+  OR: [QueryIntInput!]
+  NOR: [QueryIntInput!]
+  EQ: Int
+  NEQ: Int
+  EX: Int
+  NEX: Int
+  IN: [Int!]
+  NIN: [Int!]
+  GT: Int
+  GTE: Int
+  LT: Int
+  LTE: Int
+}
+input QueryIntItemInput {
+  AT: Int!
+  AND: [QueryIntInput!]
+  OR: [QueryIntInput!]
+  NOR: [QueryIntInput!]
+  EQ: Int
+  NEQ: Int
+  EX: Int
+  NEX: Int
+  IN: [Int!]
+  NIN: [Int!]
+  GT: Int
+  GTE: Int
+  LT: Int
+  LTE: Int
+}
+input QueryIntArrayInput {
+  SIZE: QueryIntInput
+  ITEMS: [QueryIntItemInput!]
+  AND: [QueryIntInput!]
+  OR: [QueryIntInput!]
+  NOR: [QueryIntInput!]
+  EQ: Int
+  NEQ: Int
+  EX: Int
+  NEX: Int
+  IN: [Int!]
+  NIN: [Int!]
+  GT: Int
+  GTE: Int
+  LT: Int
+  LTE: Int
+}
+
+input QueryDoc0Input {
+  str: QueryStringInput
+  bools: QueryBooleanArrayInput
+}
+
+input QueryDoc0ItemInput {
+  AT: Int!
+  str: QueryStringInput
+  bools: QueryBooleanArrayInput
+}
+
+input QueryDoc0ArrayInput {
+  SIZE: QueryIntInput
+  ITEMS: [QueryDoc0ItemInput!]
+  str: QueryStringInput
+  bools: QueryBooleanArrayInput
+}
+
+input Doc0ChangesInput {
+  str: String
+  bools: [Boolean!]
+}
+
+input Doc0UpdateInput {
+  id: ID!
+  changes: Doc0ChangesInput!
+}
+
+input Doc0Input {
+  str: String!
+  bools: [Boolean!]!
+}
+
+type Doc0 {
+  str: String!
+  bools: [Boolean!]!
+}
+
+input QueryDoc1Input {
+  str: QueryStringInput
+  thisDoc0: QueryDoc0Input
+}
+
+input QueryDoc1ItemInput {
+  AT: Int!
+  str: QueryStringInput
+  thisDoc0: QueryDoc0Input
+}
+
+input QueryDoc1ArrayInput {
+  SIZE: QueryIntInput
+  ITEMS: [QueryDoc1ItemInput!]
+  str: QueryStringInput
+  thisDoc0: QueryDoc0Input
+}
+
+input Doc1ChangesInput {
+  str: String
+  thisDoc0: Doc0ChangesInput
+}
+
+input Doc1UpdateInput {
+  id: ID!
+  changes: Doc1ChangesInput!
+}
+
+input Doc1Input {
+  str: String!
+  thisDoc0: Doc0!
+}
+
+type Doc1 {
+  str: String!
+  thisDoc0: Doc0!
+}
+
+input QueryDoc2Input {
+  str: QueryStringInput
+  doc1s: QueryDoc1ArrayInput
+}
+
+input QueryDoc2ItemInput {
+  AT: Int!
+  str: QueryStringInput
+  doc1s: QueryDoc1ArrayInput
+}
+
+input QueryDoc2ArrayInput {
+  SIZE: QueryIntInput
+  ITEMS: [QueryDoc2ItemInput!]
+  str: QueryStringInput
+  doc1s: QueryDoc1ArrayInput
+}
+
+input Doc2ChangesInput {
+  str: String
+  doc1s: [Doc1ChangesInput!]
+}
+
+input Doc2UpdateInput {
+  id: ID!
+  changes: Doc2ChangesInput!
+}
+
+input Doc2Input {
+  str: String!
+  doc1s: [Doc1!]!
+}
+
+type Doc2 {
+  str: String!
+  doc1s: [Doc1!]!
+}
+
+type Query {
+
+  getDoc0Where(query: QueryDoc0Input): [Doc0!]
+  getByIdDoc0(id: ID!): Doc0
+  getDoc1Where(query: QueryDoc1Input): [Doc1!]
+  getByIdDoc1(id: ID!): Doc1
+  getDoc2Where(query: QueryDoc2Input): [Doc2!]
+  getByIdDoc2(id: ID!): Doc2
+}
+type Mutation {
+
+  createDoc0(input: Doc0Input!): Doc0
+  updateDoc0(update: Doc0UpdateInput!): Doc0
+  deleteDoc0(id: ID!): Doc0
+  createDoc0Many(inputs: [Doc0Input!]!): [Doc0!]
+  updateDoc0Many(updates: [Doc0UpdateInput!]!): [Doc0!]
+  deleteDoc0Many(ids: [ID!]!): [Doc0]
+  updateDoc0Where(query: QueryDoc0Input!, updates: [Doc0UpdateInput!]!): [Doc0!]
+  deleteDoc0Where(query: QueryDoc0Input!, ids: [ID!]!): [Doc0]
+
+  createDoc1(input: Doc1Input!): Doc1
+  updateDoc1(update: Doc1UpdateInput!): Doc1
+  deleteDoc1(id: ID!): Doc1
+  createDoc1Many(inputs: [Doc1Input!]!): [Doc1!]
+  updateDoc1Many(updates: [Doc1UpdateInput!]!): [Doc1!]
+  deleteDoc1Many(ids: [ID!]!): [Doc1]
+  updateDoc1Where(query: QueryDoc1Input!, updates: [Doc1UpdateInput!]!): [Doc1!]
+  deleteDoc1Where(query: QueryDoc1Input!, ids: [ID!]!): [Doc1]
+
+  createDoc2(input: Doc2Input!): Doc2
+  updateDoc2(update: Doc2UpdateInput!): Doc2
+  deleteDoc2(id: ID!): Doc2
+  createDoc2Many(inputs: [Doc2Input!]!): [Doc2!]
+  updateDoc2Many(updates: [Doc2UpdateInput!]!): [Doc2!]
+  deleteDoc2Many(ids: [ID!]!): [Doc2]
+  updateDoc2Where(query: QueryDoc2Input!, updates: [Doc2UpdateInput!]!): [Doc2!]
+  deleteDoc2Where(query: QueryDoc2Input!, ids: [ID!]!): [Doc2]
+
+}
+`
